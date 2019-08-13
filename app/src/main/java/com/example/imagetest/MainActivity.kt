80c4d7a458451fe2757c025e5d58d9c27533c4b5
Andrey Antonenko <a.v.antonenko@tinkoff.ru>
2019-08-13 16:14:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/imagetest/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.imagetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import coil.api.load
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        load.setOnClickListener {
+            img.load("https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80")
+        }
+    }
+}
